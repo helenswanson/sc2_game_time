825f9431a511645d5c2efafda5fd3d55d794d34a
helenswanson <hhyswanson@gmail.com>
2014-05-26 19:15:48 -0400

    Index.erb and show.erb finalized. localhost not connecting. Version sent to Eric

--- server.rb
+++ server.rb
@@ -2,16 +2,14 @@ require 'sinatra'
 require 'csv'
 require 'pry'
 
+set :views, File.dirname(__FILE__) + '/views'
+
 #extract csv file and return array of game data
 def export_csv(filename)
   game_matchups = []
   CSV.foreach(filename, headers: true) do |row|
     game_matchups << row.to_hash
   end
-#{"home_team"=>"Patriots", "away_team"=>"Broncos", "home_score"=>"7", "away_score"=>"3"}
-#{"home_team"=>"Broncos", "away_team"=>"Colts", "home_score"=>"3", "away_score"=>"0"}
-#{"home_team"=>"Patriots", "away_team"=>"Colts", "home_score"=>"11", "away_score"=>"7"}
-#{"home_team"=>"Steelers", "away_team"=>"Patriots", "home_score"=>"7", "away_score"=>"21"}
   game_matchups
 end
 
@@ -22,43 +20,81 @@ def create_team_list(game_matchups)
   duplicate_team_list = home_team_list.concat(away_team_list)
   #remove dupicate team names from team list
   duplicate_team_list.map{|team| team}.uniq
-#["Patriots", "Broncos", "Steelers", "Colts"]
 end
 
-#create array containing hashes where team name key points to hash containing team wins/losses/ties
-def calculate_team_scores(game_matchups, team_list)
+#create scorecard_template; team key => empty hash of wins/losses/ties
+def create_scorecard_template(game_matchups, team_list)
+  scorecard_template = {}
+  team_list.map{|team| scorecard_template[team] = {wins: 0, losses: 0, ties: 0}}.uniq
+  scorecard_template
 end
 
+#TEAM_SCORECARD RETURN IS NOT A CORRECT========================
+#calculate number of wins/losses/ties for each team
+def calculate_team_scorecard(game_matchups, scorecard_template)
+  team_scorecard = scorecard_template
+  game_matchups.each do |game_info|
+    if game_info["home_score"] > game_info["away_score"]
+      #add 1 win to home_team and add 1 loss to away_team
+      team_scorecard[game_info["home_team"]][:wins] += 1
+      team_scorecard[game_info["away_team"]][:losses] += 1
+    elsif game_info["home_score"] < game_info["away_score"]
+      #add 1 win to away_team and add 1 loss to home_team
+      team_scorecard[game_info["home_team"]][:losses] += 1
+      team_scorecard[game_info["away_team"]][:wins] += 1
+    else
+      #add 1 tie to both home_team and away_team
+      team_scorecard[game_info["home_team"]][:ties] += 1
+      team_scorecard[game_info["away_team"]][:ties] += 1
+    end
+  end
+  team_scorecard.to_a
+end
+
+#sort team_scoreboard create scoreboard array of hashes, team name key with
+# an array of win/loss/tie values
+def sort_team_scorecard(team_scorecard)
+  team_scorecard
+  #sort scorecard first by ties in decending order
+  team_scorecard.sort_by! {|team_info| team_info[1][:ties]}.reverse!
+  #sort scorecard second by wins in decending order
+  team_scorecard.sort_by! {|team_info| team_info[1][:wins]}.reverse!
+  #sort scorecard last by losses in ascending order
+  team_scorecard.sort_by! {|team_info| team_info[1][:losses]}
+end
 
 #=====================================================
 
-#display homepage with clickable team links
+#game_matchups, team_list, scorecard_template, team_scorecard, sorted_scorecard
+
 get '/' do
+  redirect '/leaderboard'
+end
+
+get '/leaderboard' do
   game_matchups = export_csv('game_data.csv')
-  @team_list = create_team_list(game_matchups)
+  #remove this when team_scorecard is fixed
+  #team_scorecard = calculate_team_scorecard(game_matchups, scorecard_template)
+  team_scorecard = [["Patriots", {:wins=>3, :losses=>0, :ties=>0}],
+  ["Broncos", {:wins=>1, :losses=>1, :ties=>0}],
+  ["Steelers", {:wins=>0, :losses=>1, :ties=>0}],
+  ["Colts", {:wins=>0, :losses=>2, :ties=>0}]]
 
-
-  #@team_list = unique_teams(teams)
+  @sorted_scorecard = sort_team_scorecard(team_scorecard)
   erb :index
 end
 
 get '/teams/:team_name' do
   @team_title = params[:team_name]
-  game_matchups = export_csv('game_data.csv')
+  @game_matchups = export_csv('game_data.csv')
 
-
-
+  team_scorecard = [["Patriots", {:wins=>3, :losses=>0, :ties=>0}],
+  ["Broncos", {:wins=>1, :losses=>1, :ties=>0}],
+  ["Steelers", {:wins=>0, :losses=>1, :ties=>0}],
+  ["Colts", {:wins=>0, :losses=>2, :ties=>0}]]
+  @sorted_scorecard = sort_team_scorecard(team_scorecard)
   #array of hashes containing first_name, last_name, position for people on a specific team
   #@team_members = team_member_list(teams, @team_title)
   erb :show
 end
-
-
-
-
-
-
-
-
-
 
--- test_method.rb
+++ test_method.rb
@@ -29,64 +29,89 @@ def create_scorecard_template(game_matchups, team_list)
   scorecard_template
 end
 
-#TEAM_SCORECARD IS NOT A CORRECT========================
+#TEAM_SCORECARD RETURN IS NOT A CORRECT========================
 #calculate number of wins/losses/ties for each team
 def calculate_team_scorecard(game_matchups, scorecard_template)
   team_scorecard = scorecard_template
   #based on game results, increase appropriate wins/losses/ties values
-
   game_matchups.each do |game_info|
-    home_team = game_info["home_team"]
-    away_team = game_info["away_team"]
-    home_score = game_info["home_score"]
-    away_score = game_info["away_score"]
-
-    if home_score > away_score
+    # home_team = game_info["home_team"]
+    # away_team = game_info["away_team"]
+    # home_score = game_info["home_score"]
+    # away_score = game_info["away_score"]
+    if game_info["home_score"] > game_info["away_score"]
       #add 1 win to home_team and add 1 loss to away_team
-      team_scorecard[home_team][:wins] += 1
-      team_scorecard[away_team][:losses] += 1
-    elsif home_score < away_score
+      team_scorecard[game_info["home_team"]][:wins] += 1
+      team_scorecard[game_info["away_team"]][:losses] += 1
+    elsif game_info["home_score"] < game_info["away_score"]
       #add 1 win to away_team and add 1 loss to home_team
-      team_scorecard[home_team][:losses] += 1
-      team_scorecard[away_team][:wins] += 1
+      team_scorecard[game_info["home_team"]][:losses] += 1
+      team_scorecard[game_info["away_team"]][:wins] += 1
     else
       #add 1 tie to both home_team and away_team
-      team_scorecard[home_team][:ties] += 1
-      team_scorecard[away_team][:ties] += 1
+      team_scorecard[game_info["home_team"]][:ties] += 1
+      team_scorecard[game_info["away_team"]][:ties] += 1
     end
-    binding.pry
-
   end
-  team_scorecard
+  team_scorecard.to_a
 end
 
-#create scoreboard array of hashes, team name key with
+
+
+#sort team_scoreboard create scoreboard array of hashes, team name key with
 # an array of win/loss/tie values
-def create_scoreboard(team_list, team_scorecard)
+def sort_team_scorecard(team_scorecard)
+  team_scorecard
+  binding.pry
+  #sort scorecard first by ties in decending order
+  team_scorecard.sort_by! {|team_info| team_info[1][:ties]}.reverse!
+  binding.pry
+  #sort scorecard second by wins in decending order
+  team_scorecard.sort_by! {|team_info| team_info[1][:wins]}.reverse!
+  #sort scorecard last by losses in ascending order
+  binding.pry
+  team_scorecard.sort_by! {|team_info| team_info[1][:losses]}
 
-
+  binding.pry
 end
 
 
 #=====================================================
 
+
 game_matchups = export_csv('game_data.csv')
 puts game_matchups
 puts
 
+#create list of unique team names
 team_list = create_team_list(game_matchups)
 puts team_list
 puts
 
+#create template where team key points to hash of wins/losses/ties
 scorecard_template = create_scorecard_template(game_matchups, team_list)
 puts scorecard_template
 puts
 
 #TEAM_SCORECARD IS NOT A CORRECT=========================
+#calculate the win/loss/tie scores for each team
 team_scorecard = calculate_team_scorecard(game_matchups, scorecard_template)
 puts team_scorecard
 puts
 
-scoreboard = create_scoreboard(team_list, team_scorecard)
+#provide correct team_scorecard data
+team_scorecard = [["Broncos", {:wins=>1, :losses=>1, :ties=>0}],
+ ["Steelers", {:wins=>0, :losses=>1, :ties=>0}],
+ ["Patriots", {:wins=>3, :losses=>0, :ties=>0}],
+ ["Packers", {:wins=>1, :losses=>0, :ties=>0}],
+ ["Colts", {:wins=>0, :losses=>2, :ties=>0}],
+ ["TEAM B", {:wins=>1, :losses=>2, :ties=>0}],
+ ["TEAM C", {:wins=>3, :losses=>1, :ties=>0}]]
+puts "edited scorecard"
+puts team_scorecard
+puts
+
+#sort scorecard with best teams on top
+sorted_scorecard = sort_team_scorecard(team_scorecard)
 puts scoreboard
 puts
